a37b359f9854cc5d8148f77d7e6fdfc124a6c66a
brutzman <brutzman@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2011-05-29 20:15:42 +0000

    set local path to mounted lab directory if JAVA not found

--- testing/vaadin-testbench/mmowgli-vaadin-testbench-script.bat
+++ testing/vaadin-testbench/mmowgli-vaadin-testbench-script.bat
@@ -13,8 +13,10 @@ IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=C:\Program Files\apache-ant-1.8.2
 IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=C:\Program Files\apache-ant-1.8.1
 IF NOT EXIST  %ANT_HOME%    PAUSE Error, Ant not found!
 IF NOT EXIST  %ANT_HOME%    EXIT
-IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
+
+IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=..\java\jdk1.6.0_25
 IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_25
+IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
 IF NOT EXIST %JAVA_HOME%    PAUSE Error, Java not found!
 IF NOT EXIST %JAVA_HOME%    EXIT
 
