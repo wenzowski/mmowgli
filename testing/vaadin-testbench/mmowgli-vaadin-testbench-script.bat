780a29283933ddd06566faf5bc7ec8125dd18a86
brutzman <brutzman@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2011-05-29 05:49:36 +0000

    better PATH handling

--- testing/vaadin-testbench/mmowgli-vaadin-testbench-script.bat
+++ testing/vaadin-testbench/mmowgli-vaadin-testbench-script.bat
@@ -3,16 +3,23 @@
 @REM Authors Don Brutzman and Terry Norbraten
 @REM created 14 May 2011
 
-@REM debug these statements, probably overkill anyway:
-@REM IF NOT EXIST  %ANT_HOME%	 ANT_HOME=C:\Program Files\apache-ant-1.8.2
 @REM IF NOT EXIST %JAVA_HOME%	JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
 
 @REM	cd not needed since in same directory
 @REM	cd H:\vaadin-testbench
 
-IF NOT EXIST  %ANT_HOME% SET  ANT_HOME=C:\Program Files\apache-ant-1.8.2
-IF NOT EXIST %JAVA_HOME% SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
+@REM debug these statements, probably overkill anyway:
+IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=C:\Program Files\apache-ant-1.8.1
+IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=C:\Program Files\apache-ant-1.8.2
+IF NOT EXIST  %ANT_HOME%    @ECHO Error, Ant not found!
+IF NOT EXIST  %ANT_HOME%    EXIT
+IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
+IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_25
+IF NOT EXIST %JAVA_HOME%    @ECHO Error, Java not found!
+IF NOT EXIST %JAVA_HOME%    EXIT
+
 SET Path=%ANT_HOME%\bin;%JAVA_HOME%\bin;%Path%;
+@ECHO PATH=%PATH%
 
 @ECHO Clearing cache Internet Explorer...
 RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255 
