d0f5dca898a8253b9a561efcee36f4d2f4ea5955
brutzman <brutzman@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2011-05-29 19:48:52 +0000

    establish local path, pause to show results afterwards

--- testing/vaadin-testbench/kill-java-script.bat
+++ testing/vaadin-testbench/kill-java-script.bat
@@ -10,11 +10,22 @@
 @REM	cd not needed since in same directory
 @REM	cd H:\vaadin-testbench
 
-IF NOT EXIST  %ANT_HOME% SET  ANT_HOME=C:\Program Files\apache-ant-1.8.2
-IF NOT EXIST %JAVA_HOME% SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
-SET Path=%ANT_HOME%\bin;%JAVA_HOME%\bin;%Path%;
+IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=.\apache-ant-1.8.2
+IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=C:\Program Files\apache-ant-1.8.2
+IF NOT EXIST  %ANT_HOME%    SET  ANT_HOME=C:\Program Files\apache-ant-1.8.1
+IF NOT EXIST  %ANT_HOME%    @ECHO Error, Ant not found!
+IF NOT EXIST  %ANT_HOME%    EXIT
+@ECHO ANT_HOME=%ANT_HOME%
 
-@ECHO killing all running java tasks
+IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_24
+IF NOT EXIST %JAVA_HOME%    SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_25
+IF NOT EXIST %JAVA_HOME%    @ECHO Error, Java not found!
+IF NOT EXIST %JAVA_HOME%    EXIT
+
+SET Path=%ANT_HOME%\bin;%JAVA_HOME%\bin;%Path%;
+@ECHO PATH=%PATH%
+
+@ECHO  kill all running java tasks:
 start "kill.all.java"	ant kill.all.java
 
 REM pause 5 seconds
@@ -22,5 +33,6 @@ PING 1.1.1.1 -n 1 -w 5000 > NUL
 
 TASKLIST
 
-@REM PAUSE to inspect console output
+PAUSE to inspect console output, press any key to exit
+
 @REM exit
