0b8491cc5785a141654c146720bd75ae22ddca58
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-09-16 22:47:41 +0000

    Final commit candidate

--- mmowgli2/src/edu/nps/moves/mmowgli/modules/gamemaster/GameEventLogger.java
+++ mmowgli2/src/edu/nps/moves/mmowgli/modules/gamemaster/GameEventLogger.java
@@ -39,15 +39,12 @@ import java.io.Serializable;
 import java.net.InetAddress;
 import java.util.Set;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
 import edu.nps.moves.mmowgli.AppMaster;
 import edu.nps.moves.mmowgli.Mmowgli2UI;
 import edu.nps.moves.mmowgli.db.*;
 import edu.nps.moves.mmowgli.db.GameEvent.EventType;
-import edu.nps.moves.mmowgli.hibernate.*;
-import edu.nps.moves.mmowgli.utility.M;
+import edu.nps.moves.mmowgli.hibernate.DBGet;
+import edu.nps.moves.mmowgli.hibernate.HSess;
 
 /**
  * GameEventLogger.java
@@ -65,7 +62,7 @@ public class GameEventLogger
   private static String svrName;
   private static int SVRNAME_LIMIT = 5;
   static {
-    svrName = AppMaster.getInstance().getServerName();
+    svrName = AppMaster.instance().getServerName();
     if(svrName == null)
       svrName = "";
     else {
@@ -76,41 +73,10 @@ public class GameEventLogger
         svrName = svrName.substring(0,SVRNAME_LIMIT);        
     }
   }
-//  public static         case USERUPDATE: return "User data updated -- score, info";
-  /*
-   *     // Log
-    User u = DBGet.getUser(uId,sess);
-    GameEvent ge = new GameEvent(GameEvent.EventType.USERUPDATE,"user "+u.getUserName());
-    sess.save(ge);
 
-   */
-//  case IDEACARDPLAYED: return "Innovate or Defend car played";
-//  case CHILDCARDPLAYED: return "Card played on a parent card";
-  /*
-   *     
-    // Log
-    Card cd = DBGet.getCard(cardId,sess); 
-    CardType ct = cd.getCardType();
-    GameEvent ge;
-    String msg = " by "+cd.getAuthorName()+" / "+cd.getText();
-    if(ct.isIdeaCard())
-      ge = new GameEvent(GameEvent.EventType.IDEACARDPLAYED, msg);
-    else
-      ge = new GameEvent(GameEvent.EventType.CHILDCARDPLAYED,msg);
-    sess.save(ge);
-
-   */
-  
-  public static void cardPlayed(Object cid)
+  public static void cardPlayedTL(Object cid)
   {
-    cardPlayed_oob(VHib.instance(), cid);
-  }
-
-  public static void cardPlayed_oob(SessionManager mgr, Object cid)
-  {
-    Session sess = M.getSession(mgr);
-
-    Card cd = DBGet.getCard(cid, sess);
+    Card cd = DBGet.getCardTL(cid);
     CardType ct = cd.getCardType();
     GameEvent ge;
     StringBuilder sb = new StringBuilder();
@@ -135,34 +101,22 @@ public class GameEventLogger
     else
       ge = new GameEvent(GameEvent.EventType.CHILDCARDPLAYED, sb.toString());
 
-    Sess.sessOobSave(sess, ge);
-    if(mgr instanceof SingleSessionManager)
-      ((SingleSessionManager)mgr).setNeedsCommit(true);    
+    GameEvent.saveTL(ge);;
   }
 
-  public static void cardTextEditted(Object cid, Object uid)
+  public static void cardTextEdittedTL(Object cid, Object uid)
   {
-    cardTextEditted_oob(VHib.instance(), cid, uid);
+    cardChangedCommonTL(GameEvent.EventType.CARDTEXTEDITED,cid,uid);
   }
-  public static void cardTextEditted_oob(SessionManager mgr, Object cid, Object uid)
+   
+  public static void cardMarkedTL(Object cid, Object uid)
   {
-    cardChangedCommon_oob(GameEvent.EventType.CARDTEXTEDITED,mgr,cid,uid);    
-  }
-  
-  public static void cardMarked(Object cid, Object uid)
-  {
-    cardMarked_oob(VHib.instance(), cid, uid);
-  }
-  public static void cardMarked_oob(SessionManager mgr, Object cid, Object uid)
-  {
-    cardChangedCommon_oob(GameEvent.EventType.CARDMARKED,mgr,cid,uid);
+    cardChangedCommonTL(GameEvent.EventType.CARDMARKED,cid,uid);
   }
   
-  private static void cardChangedCommon_oob(GameEvent.EventType typ, SessionManager mgr, Object cid, Object uid)
+  private static void cardChangedCommonTL(GameEvent.EventType typ, Object cid, Object uid)
   {
-    Session sess = M.getSession(mgr);
-
-    Card c = DBGet.getCard(cid, sess);
+    Card c = DBGet.getCardTL(cid);
     
     Set<CardMarking> cm = c.getMarking();
     StringBuilder sb = new StringBuilder();
@@ -171,7 +125,7 @@ public class GameEventLogger
     sb.append(" / ");
     sb.append("card "+c.getId());
     sb.append(" / user ");
-    User marker = DBGet.getUser(uid,sess);
+    User marker = DBGet.getUserTL(uid);
     sb.append(marker.getId());
     //sb.append(marker.getUserName());
     sb.append(" / ");
@@ -183,51 +137,32 @@ public class GameEventLogger
     sb.append(c.getText());
     
     GameEvent ev = new GameEvent(typ,sb.toString());
-    Sess.sessOobSave(sess, ev);
-    
-    if(mgr instanceof SingleSessionManager)
-      ((SingleSessionManager)mgr).setNeedsCommit(true);    
+    GameEvent.saveTL(ev);;
   }
   
-  public static void logGameDesignChange(String field, String value, Object uid)
+  public static void logGameDesignChangeTL(String field, String value, Object uid)
   {
-    User u = DBGet.getUser(uid);
+    User u = DBGet.getUserTL(uid);
     GameEvent ev = new GameEvent(GameEvent.EventType.GAMEDESIGNEDITED,"/ "+svrName+" user "+u.getId()+ " / "+field+" / "+value);
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
   
-  public static void logUserScoreChanged(Object uid)
-  {
-    logUserScoreChanged(VHib.instance(),uid);
-  }
-  
-  public static void logUserNameChanged(Object uid, Object changer, String oldName, String newName)
+  public static void logUserNameChangedTL(Object uid, Object changer, String oldName, String newName)
   {
     GameEvent ev = new GameEvent(GameEvent.EventType.USERGAMENAMECHANGED,"/ "+svrName+" userId "+uid+" old: "+oldName+" new: "+newName+" by userId "+changer);    
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
   
-  public static void logUserScoreChanged(SessionManager mgr, Object uid)
+  public static void logHelpWantedTL(Object apId)
   {
-    Session sess = M.getSession(mgr);
-    User u = DBGet.getUser(uid,sess);
-    GameEvent ev = new GameEvent(GameEvent.EventType.SCORECHANGE,"/ "+svrName+" user "+u.getId());
-    Sess.sessOobSave(sess, ev);
-    
-    if(mgr instanceof SingleSessionManager)
-      ((SingleSessionManager)mgr).setNeedsCommit(true);    
-  }
-  
-  public static void logHelpWanted(Object apId)
-  {
-    ActionPlan ap = ActionPlan.get(apId);
+    ActionPlan ap = ActionPlan.getTL(apId);
     Serializable uid = Mmowgli2UI.getGlobals().getUserID();
-    User me = DBGet.getUser(uid);
+    User me = DBGet.getUserTL(uid);
     String s = ap.getHelpWanted();
     if(s == null)
       s = "(removed)";
     GameEvent ev = new GameEvent(GameEvent.EventType.ACTIONPLANHELPWANTED,"/ "+apId+" / user "+me.getId()+" / "+s);
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
   
   private static String getUserString(GameEvent.EventType typ, User u)
@@ -240,7 +175,7 @@ public class GameEventLogger
     sb.append(" from ");
     sb.append(u.getLocation());
     sb.append(" / ");
-    String browAddr = AppMaster.getInstance().browserAddress();
+    String browAddr = AppMaster.instance().browserAddress();
     sb.append(browAddr==null?"null":browAddr);
     
     if(typ == GameEvent.EventType.USERLOGIN && u.isViewOnly())  // guest
@@ -249,41 +184,36 @@ public class GameEventLogger
     return sb.toString();
   }
   
-  public static void logUserLogin(Object uid)
+  public static void logUserLoginTL(Object uid)
   {
-    Session sess = VHib.getVHSession();
-    User u = DBGet.getUser(uid,sess);
+    User u = DBGet.getUserTL(uid);
     GameEvent ev = new GameEvent(GameEvent.EventType.USERLOGIN, " "+getUserString(GameEvent.EventType.USERLOGIN,u));
-    Sess.sessOobSave(sess, ev);
+    GameEvent.saveTL(ev);;
   }
 
-  public static void logUserLogout(Session sess, Object uid)
+  public static void logUserLogoutTL(Object uid)
   {
-    User u = DBGet.getUser(uid,sess);
+    User u = DBGet.getUserTL(uid);
     GameEvent ev = new GameEvent(GameEvent.EventType.USERLOGOUT, " "+getUserString(GameEvent.EventType.USERLOGOUT,u));
-    Sess.sessOobSave(sess, ev);
+    GameEvent.saveTL(ev);
   }
-  public static void logUserLogout(Object uid)
-  {
-    logUserLogout(VHib.getVHSession(),uid);
-  }
-  
-  public static void logLoginLimitChange(int old, int newly)
+
+  public static void logLoginLimitChangeTL(int old, int newly)
   {
     GameEvent ev = new GameEvent(GameEvent.EventType.LOGINLIMITCHANGE," / old: "+old+" new: "+newly);
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
   
-  public static void logGameMasterComment(String comment, User u)
+  public static void logGameMasterCommentTL(String comment, User u)
   {
     GameEvent ev = new GameEvent(GameEvent.EventType.GAMEMASTERNOTE,"/  "+svrName+" (user "+u.getId()+") "+comment);
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
-  
-  public static void logGameMasterBroadcast(EventType typ, String msg, User u)
+ 
+  public static void logGameMasterBroadcastTL(EventType typ, String msg, User u)
   {
     GameEvent ev = new GameEvent(typ," From user "+u.getId()+": "+msg);
-    GameEvent.save(ev);   
+    GameEvent.saveTL(ev);   
   }
   
   public static void logApplicationLaunch()
@@ -298,28 +228,14 @@ public class GameEventLogger
     {
       System.err.println("Can't look up host name in GameEventLogger");
     }
-
-    Session sess = VHib.openSession();
-    Transaction tx = sess.beginTransaction();
-    tx.setTimeout(HIBERNATE_TRANSACTION_TIMEOUT_IN_SECONDS);
+    HSess.init();
+    HSess.get().getTransaction().setTimeout(HIBERNATE_TRANSACTION_TIMEOUT_IN_SECONDS);
     GameEvent ev = new GameEvent(GameEvent.EventType.APPLICATIONSTARTUP,"/ "+SERVERNAME);
-    Sess.sessOobSave(sess,ev);
-    tx.commit();
-    sess.close();   
+    GameEvent.saveTL(ev);
+    HSess.close();   
   }
 
-  public static void xlogActionPlanUpdate(ActionPlan ap, String field, String authorname)
-  {
-    String title = ap.getTitle();
-    title = (title==null?"":title);
-    if(title.length() > 70)
-      title = title.substring(0, 69)+"...";
-        
-    GameEvent ev = new GameEvent(GameEvent.EventType.ACTIONPLANUPDATED," / action plan "+ap.getId()+" by "+authorname+", "+field);
-    GameEvent.save(ev);    
-  }
-  
-  public static void logActionPlanUpdate(ActionPlan ap, String field, long id)
+  public static void logActionPlanUpdateTL(ActionPlan ap, String field, long id)
   {
     String title = ap.getTitle();
     title = (title==null?"":title);
@@ -327,43 +243,32 @@ public class GameEventLogger
       title = title.substring(0, 69)+"...";
         
     GameEvent ev = new GameEvent(GameEvent.EventType.ACTIONPLANUPDATED," / action plan "+ap.getId()+" by user "+id+", "+field);
-    GameEvent.save(ev);    
+    GameEvent.saveTL(ev);    
   }
   
-  public static void logSessionEnd(Object uId, SingleSessionManager sessMgr)
-  {
-    Session sess = M.getSession(sessMgr);
-    User u = DBGet.getUser(uId,sess);
-    GameEvent ev = new GameEvent(GameEvent.EventType.SESSIONEND," "+svrName+" / user "+u.getId()+" / "+u.getLocation());
-    Sess.sessOobSave(sess, ev);
-    sessMgr.setNeedsCommit(true);
-  }
+//  public static void logSessionEnd(Object uId, SingleSessionManager sessMgr)
+//  {
+//    Session sess = M.getSession(sessMgr);
+//    User u = DBGet.getUser(uId,sess);
+//    GameEvent ev = new GameEvent(GameEvent.EventType.SESSIONEND," "+svrName+" / user "+u.getId()+" / "+u.getLocation());
+//    Sess.sessOobSave(sess, ev);
+//    sessMgr.setNeedsCommit(true);
+//  }
 
-  public static void logRegistrationAttempt(String email)
+  public static void logRegistrationAttemptTL(String email)
   {
     GameEvent ev = new GameEvent(GameEvent.EventType.REGISTRATIONATTEMPT, " by <a href='mailto:"+email+"'>"+email+"</a>");
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
   
-  public static void xupdateBlogHeadline(String txt, String tooltip, String url, String uname)
-  {
-    MessageUrl mu = new MessageUrl(txt,url);
-    mu.setTooltip(tooltip);
-    MessageUrl.save(mu);
-    long id = mu.getId();
-    txt = txt==null?" (removed)":txt;
-    GameEvent ev = new GameEvent(GameEvent.EventType.BLOGHEADLINEPOST," by "+uname+" / "+txt,id);
-    GameEvent.save(ev);
-  }
-  
-  public static void updateBlogHeadline(String txt, String tooltip, String url, long uid)
+  public static void updateBlogHeadlineTL(String txt, String tooltip, String url, long uid)
   {
     if(url!=null && url.length()>255)  // db limit
       url = url.substring(0, 255);
     
     MessageUrl mu = new MessageUrl(txt,url);
     mu.setTooltip(tooltip);
-    MessageUrl.save(mu);
+    MessageUrl.saveTL(mu);
     long id = mu.getId();
     txt = txt==null?" (removed)":txt;
     tooltip = tooltip==null?"":tooltip;
@@ -374,37 +279,31 @@ public class GameEventLogger
     sb.append(" / (url:) ");
     sb.append(url);
     GameEvent ev = new GameEvent(GameEvent.EventType.BLOGHEADLINEPOST," by user "+uid+" / "+ sb.toString(),id);
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
 
-  public static void logEndReportGeneration()
+  public static void logEndReportGenerationTL()
   {
-    SingleSessionManager ssm = new SingleSessionManager();
-    logReportGeneration(ssm, " completed");
-    ssm.endSession();
+    logReportGenerationTL(" completed");
   }
 
-  public static void logBeginReportGeneration()
+  public static void logBeginReportGenerationTL()
   {
-    SingleSessionManager ssm = new SingleSessionManager();
-    logReportGeneration(ssm, " begun");  
-    ssm.endSession();
+    logReportGenerationTL(" begun");  
   }
   
-  private static void logReportGeneration(SingleSessionManager sessMgr, String txt)
+  private static void logReportGenerationTL(/*SingleSessionManager sessMgr, */String txt)
   {
-    String url = AppMaster.getInstance().getAppUrlString();
+    String url = AppMaster.instance().getAppUrlString();
     if(!url.endsWith("/"))
       url = url+"/";
     txt = txt + " "+url+"reports";
     
     GameEvent ev = new GameEvent(GameEvent.EventType.AUTOREPORTGENERATION, txt);
-    
-    Sess.sessOobSave(M.getSession(sessMgr), ev);
-    sessMgr.setNeedsCommit(true);        
+    GameEvent.saveTL(ev);;       
   }
 
-  public static void logNewUser(User user)
+  public static void logNewUserTL(User user)
   {
     StringBuilder sb = new StringBuilder();
     sb.append(" / user ");
@@ -417,10 +316,10 @@ public class GameEventLogger
     sb.append(user.getAnswer());
     sb.append("\"");
     GameEvent ev = new GameEvent(GameEvent.EventType.USERNEW, sb.toString());
-    GameEvent.save(ev);   
+    GameEvent.saveTL(ev);   
   }
 
-  public static void commentMarkedSuperInteresting(String user, Object apId, Message msg, boolean superInteresting)
+  public static void commentMarkedSuperInterestingTL(String user, Object apId, Message msg, boolean superInteresting)
   {
     StringBuilder sb = new StringBuilder(user);
     sb.append(" / marked a comment in action plan ");
@@ -428,10 +327,10 @@ public class GameEventLogger
     sb.append(superInteresting?"":" NOT");
     sb.append(" super-interesting");
     GameEvent ev = new GameEvent(GameEvent.EventType.COMMENTSUPERINTERESTING,sb.toString());
-    GameEvent.save(ev);    
+    GameEvent.saveTL(ev);    
   }
 
-  public static void commentTextEditted(String userName, Object id, Message msg)
+  public static void commentTextEdittedTL(String userName, Object id, Message msg)
   {
     StringBuilder sb = new StringBuilder(userName);
     sb.append(" edited a comment in action plan ");
@@ -439,10 +338,10 @@ public class GameEventLogger
     sb.append(": ");
     sb.append(msg.getText());
     GameEvent ev = new GameEvent(GameEvent.EventType.COMMENTEDITED,sb.toString());
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
   
-  public static void chatTextEditted(String userName, Object id, Message msg)
+  public static void chatTextEdittedTL(String userName, Object id, Message msg)
   {
     StringBuilder sb = new StringBuilder(userName);
     sb.append(" edited a chat in action plan ");
@@ -450,15 +349,15 @@ public class GameEventLogger
     sb.append(": ");
     sb.append(msg.getText());
     GameEvent ev = new GameEvent(GameEvent.EventType.CHATEDITED,sb.toString());
-    GameEvent.save(ev);
+    GameEvent.saveTL(ev);
   }
 
   /**
    * @param user
    */
-  public static void logUserPasswordChanged(User user)
+  public static void logUserPasswordChangedTL(User user)
   {
-    // TODO Auto-generated method stub
+    // TODO 
     
   }
 }
