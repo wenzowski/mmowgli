259b7ed515ab9e557bf460d9c986a97592394efa
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-09-17 20:25:34 +0000

    Final commit candidate

--- mmowgli2/src/edu/nps/moves/mmowgli/export/ReportGenerator.java
+++ mmowgli2/src/edu/nps/moves/mmowgli/export/ReportGenerator.java
@@ -40,13 +40,11 @@ import java.util.Date;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 
-import org.hibernate.Session;
-
 import edu.nps.moves.mmowgli.AppMaster;
 import edu.nps.moves.mmowgli.MmowgliConstants;
 import edu.nps.moves.mmowgli.db.Game;
 import edu.nps.moves.mmowgli.export.BaseExporter.ExportProducts;
-import edu.nps.moves.mmowgli.hibernate.SingleSessionManager;
+import edu.nps.moves.mmowgli.hibernate.HSess;
 import edu.nps.moves.mmowgli.modules.gamemaster.GameEventLogger;
 import edu.nps.moves.mmowgli.utility.MiscellaneousMmowgliTimer.MSysOut;
 
@@ -69,21 +67,17 @@ public class ReportGenerator implements Runnable
   boolean poked  = false;
 
   private long INITIAL_SLEEP_PERIOD = 1 * 60 * 1000; // 1 minute
- // private long TICK_PERIOD = 1 * 60 * 1000; // 1 minute
 
   private long reportPeriod;
   private Long lastReportMS = null;
 
   DateFormat df = DateFormat.getInstance();
-  //DateFormat tickFormat = new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss.SSS");
 
   public final String INDEX_STYLESHEET_NAME = "/edu/nps/moves/mmowgli/export/ReportsIndex.xsl";
 
-  private AppMaster appMaster;
-
   public ReportGenerator(AppMaster appMaster)
   {
-    this.appMaster = appMaster;
+    //this.appMaster = appMaster;
     thread = new Thread(this,"ReportGeneratorThread");
     thread.setPriority(Thread.NORM_PRIORITY);
     thread.setDaemon(true);
@@ -110,12 +104,10 @@ public class ReportGenerator implements Runnable
     sleep(INITIAL_SLEEP_PERIOD);
 
     while (!killed) {
-      // Get report period (which can be changed during game play
-      SingleSessionManager ssm = new SingleSessionManager();
-      Session sess = ssm.getSession();
-      Game g = (Game) sess.get(Game.class, 1L);
+      HSess.init();
+      Game g = Game.getTL();
+      // Get report period (which can be changed during game play, or even while a generation run is happening)
       reportPeriod = g.getReportIntervalMinutes() * 60 * 1000;
-      ssm.endSession();
 
       // Do the report?
       long nowMS = System.currentTimeMillis();
@@ -128,50 +120,44 @@ public class ReportGenerator implements Runnable
         poked = false;
 
         try {
-          ssm = new SingleSessionManager();
-          sess = ssm.getSession();
-          g = (Game) sess.get(Game.class, 1L);
+          g = Game.getTL();
           reportPeriod = g.getReportIntervalMinutes() * 60 * 1000;
-          ssm.endSession();
           
           MSysOut.println("Mmowgli report-generator generating card visualizer and action plan, card, user and game design reports to " + MmowgliConstants.REPORTS_FILESYSTEM_PATH + " at "
                                + df.format(new Date()));
-
+          GameEventLogger.logBeginReportGenerationTL();
           // don't put in game log...too many GameEventLogger.logBeginReportGeneration(ssm);
           MSysOut.println("Mmowgli report-generator building card visualizer");
           new CardVisualizerBuilder().build();
           
           MSysOut.println("Mmowgli report-generator exporting action plans");
-          exportOne(new ActionPlanExporter());
+          exportOneTL(new ActionPlanExporter());
           MSysOut.println("Mmowgli report-generator exporting cards");
-          exportOne(new CardExporter());
+          exportOneTL(new CardExporter());
           MSysOut.println("Mmowgli report-generator exporting users");
-          exportOne(new UserExporter());  // keep this last, uses the product of the 2 before
+          exportOneTL(new UserExporter());  // keep this last, uses the product of the 2 before
           MSysOut.println("Mmowgli report-generator exporting game");
-          String gXmlPath = exportOne(new GameExporter());
+          String gXmlPath = exportOneTL(new GameExporter());
 
           writeIndexDotHtml(MmowgliConstants.REPORTS_FILESYSTEM_PATH + "index.html", gXmlPath);
 
           MSysOut.println("Mmowgli report-generator finished");
-          GameEventLogger.logEndReportGeneration();
+          GameEventLogger.logEndReportGenerationTL();
         } // try
 
         catch (Throwable t) {
-
-          if (sess != null)
-            ssm.endSession();
-          sess = null;
-          ssm = null;
-
           if (!(t instanceof InterruptedException) && !killed) {
             System.err.println("Exception in ReportGenerator: " + t.getClass().getSimpleName() + " / " + t.getLocalizedMessage());
             t.printStackTrace();
           }
           System.err.println("ReportGenerator shutting down.");
           killed = true;
+          HSess.close();
           return; // kills thread
         }
       } // if(poked || reportPeriod == 0)
+      
+      HSess.close();
       lastReportMS = System.currentTimeMillis();
 
       long sleepTime = reportPeriod<=0?Long.MAX_VALUE:reportPeriod;
@@ -179,9 +165,9 @@ public class ReportGenerator implements Runnable
     }
   }
 
-  private String exportOne(BaseExporter exp) throws Throwable
+  private String exportOneTL(BaseExporter exp) throws Throwable
   {
-    ExportProducts exProd = exp.exportToRepository();
+    ExportProducts exProd = exp.exportToRepositoryTL();
     String fileName = exp.buildFileName(exp.getFileNamePrefix());
 
     String xmlName = fileName + ".xml";
