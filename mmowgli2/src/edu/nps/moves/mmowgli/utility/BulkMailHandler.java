6ba5d179d6b8aada141edb24798c3723a90c036f
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-09-17 20:54:09 +0000

    Final commit candidate

---
+++ mmowgli2/src/edu/nps/moves/mmowgli/utility/BulkMailHandler.java
@@ -0,0 +1,15 @@
+package edu.nps.moves.mmowgli.utility;
+
+import edu.nps.moves.mmowgli.components.SignupsTable.QueryContainer;
+
+public class BulkMailHandler
+{
+  public static void showDialog(QueryContainer cont)
+  {
+    
+  }
+  private BulkMailHandler()
+  {
+    
+  }
+}
