ddcc3d9b058fbb1f84ec1a8403d6105ec2d6e854
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-09-17 21:55:31 +0000

    An attempt to control used of the thread-local sessions

---
+++ mmowgli2/src/edu/nps/moves/mmowgli/markers/HibernateUpdate.java
@@ -0,0 +1,9 @@
+package edu.nps.moves.mmowgli.markers;
+
+/**
+ * This is a method marker for methods that use Hibernate.
+ * 
+ *@author jmbailey@nps.edu
+ */
+
+public @interface HibernateUpdate {}
---
+++ mmowgli2/src/edu/nps/moves/mmowgli/markers/MmowgliCodeEntry.java
@@ -0,0 +1,11 @@
+package edu.nps.moves.mmowgli.markers;
+
+/**
+ * This is a method marker for methods that are entered from Vaadin.  Theoretically this would indicate that
+ * we could deal with, e.g., Hibernate sessions at entry and exit of this method.  Whether this is done in
+ * mmowgli is to be determined.
+ * 
+ *@author jmbailey@nps.edu
+ */
+
+public @interface MmowgliCodeEntry {}
