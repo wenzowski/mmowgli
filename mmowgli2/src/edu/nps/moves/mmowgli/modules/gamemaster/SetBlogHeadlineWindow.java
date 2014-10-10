e2e06dbe2332a7ccb4d13b60c7553447ba37d144
jmbailey <jmbailey@nps.edu>
2014-10-10 18:04:52 +0000

    formatting

--- mmowgli2/src/edu/nps/moves/mmowgli/modules/gamemaster/SetBlogHeadlineWindow.java
+++ mmowgli2/src/edu/nps/moves/mmowgli/modules/gamemaster/SetBlogHeadlineWindow.java
@@ -1,10 +1,10 @@
 /*
 * Copyright (c) 1995-2010 held by the author(s).  All rights reserved.
-*  
+*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
 * are met:
-*  
+*
 *  * Redistributions of source code must retain the above copyright
 *       notice, this list of conditions and the following disclaimer.
 *  * Redistributions in binary form must reproduce the above copyright
@@ -17,7 +17,7 @@
 *       nor the names of its contributors may be used to endorse or
 *       promote products derived from this software without specific
 *       prior written permission.
-*  
+*
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -94,7 +94,7 @@ public class SetBlogHeadlineWindow extends Window implements ItemClickListener
     layout.setSizeFull();
     setContent(layout);
     
-    layout.addComponent(textLab=new Label("Enter headline:"));    
+    layout.addComponent(textLab=new Label("Enter headline:"));
     textTF = new TextField();
     textTF.setInputPrompt("Enter new headline or choose from previous ones below");
     textTF.setWidth("100%");
@@ -186,7 +186,7 @@ public class SetBlogHeadlineWindow extends Window implements ItemClickListener
     @Override
     public void valueChange(final ValueChangeEvent event)
     {
-       setEnabledFromCheckBox();     
+       setEnabledFromCheckBox();
     }
   }
 }
