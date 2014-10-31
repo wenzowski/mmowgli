9fe2e349ffa36e9d9d9973dec8e8d076555476e3
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-10-31 22:50:18 +0000

    not used

---
+++ mmowgli2/src/edu/nps/moves/mmowgliMobile/unused/CardRenderer.java
@@ -0,0 +1,151 @@
+package edu.nps.moves.mmowgliMobile.unused;
+
+import java.text.SimpleDateFormat;
+
+import com.vaadin.addon.touchkit.ui.NavigationButton;
+import com.vaadin.addon.touchkit.ui.NavigationButton.NavigationButtonClickEvent;
+import com.vaadin.addon.touchkit.ui.NavigationManager;
+import com.vaadin.data.Container;
+import com.vaadin.event.LayoutEvents.LayoutClickEvent;
+import com.vaadin.event.LayoutEvents.LayoutClickListener;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+import edu.nps.moves.mmowgli.db.Card;
+import edu.nps.moves.mmowgli.db.CardType;
+import edu.nps.moves.mmowgli.modules.cards.CardStyler;
+import edu.nps.moves.mmowgli.utility.MediaLocator;
+import edu.nps.moves.mmowgliMobile.data.*;
+import edu.nps.moves.mmowgliMobile.ui.*;
+
+/**
+ * CardRenderer.java Created on Feb 24, 2014
+ * 
+ * MOVES Institute Naval Postgraduate School, Monterey, CA, USA www.nps.edu
+ * 
+ * @author Mike Bailey, jmbailey@nps.edu
+ * @version $Id$
+ */
+public class CardRenderer extends EntryRenderer //implements ClickListener
+{
+  private SimpleDateFormat formatter = new SimpleDateFormat("M/d/yy hh:mm");
+  private static MediaLocator mediaLocator = new MediaLocator();
+  
+  public void setMessage(FullEntryView mView, ListEntry message, ListView messageList, AbstractOrderedLayout layout)
+  {
+    CardListEntry wc = (CardListEntry) message;
+    Card c = wc.getCard();
+    CardType typ = c.getCardType();
+    
+    layout.removeAllComponents();
+    layout.setSpacing(true);
+    
+    VerticalLayout cardLay = new VerticalLayout();
+    cardLay.addStyleName("m-card-render");
+    cardLay.setWidth("98%"); //100%");
+    cardLay.setSpacing(true);
+    layout.addComponent(cardLay);
+    
+    HorizontalLayout horl = new HorizontalLayout();
+    horl.addStyleName("m-card-header");
+    String stl = CardStyler.getCardBaseStyle(typ);
+    horl.addStyleName(stl);
+    horl.addStyleName(CardStyler.getCardTextColorOverBaseStyle(typ));
+    horl.setMargin(true);
+    horl.setWidth("100%");
+    
+    Label lbl = new Label(typ.getTitle());//c.getText());
+    horl.addComponent(lbl);
+    lbl = new Label(""+getPojoId(message));
+    lbl.addStyleName("m-text-align-right");
+    horl.addComponent(lbl);
+    cardLay.addComponent(horl);
+    
+    horl = new HorizontalLayout();
+    horl.setWidth("100%");
+    horl.setMargin(true);
+    cardLay.addComponent(horl);    
+    lbl = new Label(c.getText());
+    horl.addComponent(lbl);
+    
+    horl = new HorizontalLayout();
+    horl.addStyleName("m-card-footer");
+    
+    horl.setMargin(true);
+    horl.setWidth("100%");
+    horl.addComponent(lbl=new Label(""));
+    lbl.setWidth("5px");
+
+    Image img = new Image();
+    img.setSource(mediaLocator.locate(c.getAuthor().getAvatar().getMedia()));
+    img.setWidth("30px");
+    img.setHeight("30px");
+    horl.addComponent(img);
+       
+    horl.addComponent(lbl=new Label(c.getAuthorName()));
+    lbl.setWidth("100%");
+    lbl.addStyleName("m-text-align-center");
+    horl.setComponentAlignment(lbl, Alignment.MIDDLE_CENTER);
+    horl.setExpandRatio(lbl, 1.0f);
+    horl.addComponent(lbl=new Label(formatter.format(message.getTimestamp()),ContentMode.HTML));
+    lbl.setWidth("115px");;
+    lbl.addStyleName("m-text-align-right");
+    horl.setComponentAlignment(lbl, Alignment.MIDDLE_CENTER);
+
+    cardLay.addComponent(horl);
+    
+//    lbl = new Label("<hr/>", ContentMode.HTML);   
+//    layout.addComponent(lbl);
+    
+    lbl = new Label("Child Cards");
+    layout.addComponent(lbl);
+    lbl.addStyleName("m-text-center");
+    
+//    lbl = new Label("<hr/>", ContentMode.HTML);
+//    layout.addComponent(lbl);
+
+    horl = new HorizontalLayout();
+    horl.setSpacing(true);
+    horl.setMargin(true);
+    horl.setWidth("100%");
+    layout.addComponent(horl);
+      
+    horl.addComponent(makeChildGroupButton("Expand", (CardListEntry) message, CardType.getExpandType(MobileVHib.getVHSession()), messageList));
+    horl.addComponent(makeChildGroupButton("Counter", (CardListEntry) message, CardType.getCounterType(MobileVHib.getVHSession()), messageList));
+    horl.addComponent(makeChildGroupButton("Adapt", (CardListEntry) message, CardType.getAdaptType(MobileVHib.getVHSession()), messageList));
+    horl.addComponent(makeChildGroupButton("Explore", (CardListEntry) message, CardType.getExploreType(MobileVHib.getVHSession()), messageList));
+  }
+
+  @SuppressWarnings("serial")
+  private Component makeChildGroupButton(final String title, CardListEntry card, CardType typ, final ListView currentMessageList)
+  {
+     final Button btn = new Button();
+     btn.addStyleName("m-card-child-button");
+     btn.addStyleName(CardStyler.getCardTextColorOverBaseStyle(typ));
+     btn.addStyleName(CardStyler.getCardBaseStyle(typ));
+    
+     final Card parent = card.getCard();
+     final Container container = new ChildCardsByTypeContainer<Card>(parent, typ);
+     if (container.size() > 0) {
+       btn.addClickListener(new ClickListener() {
+         @Override
+         public void buttonClick(ClickEvent event)
+         {
+           NavigationManager nav = currentMessageList.getNavigationManager();
+           if(nav == null)
+             nav = currentMessageList.getNavigationManager();
+           String par = parent==null?"?":(""+parent.getId());
+           nav.navigateTo(new ListView((MmowgliMobileNavManager)nav, new Folder(title+"s on card "+par, container, Card.class)));
+         }
+       });
+       btn.setCaption(title+" "+container.size());
+     }
+     else {
+       btn.setCaption(title);
+       btn.setEnabled(false); 
+     }
+     return btn;
+  } 
+}
