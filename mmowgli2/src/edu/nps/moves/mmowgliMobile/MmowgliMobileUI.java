4027d6ff85b00f09f3aa4e8a4d43f4caee35a265
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-10-31 23:12:46 +0000

    Refactor

--- mmowgli2/src/edu/nps/moves/mmowgliMobile/MmowgliMobileUI.java
+++ mmowgli2/src/edu/nps/moves/mmowgliMobile/MmowgliMobileUI.java
@@ -1,59 +1,61 @@
-/* 
- * Copyright 2009 IT Mill Ltd.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+/*
+  Copyright (C) 2014 Modeling Virtual Environments and Simulation (MOVES)
+  Institute at the Naval Postgraduate School (NPS)
+  (http://www.MovesInstitute.org and http://www.nps.edu)
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program, in the form of files named COPYING and
+  COPYING.LESSER.  If not, see <http://www.gnu.org/licenses/>
+*/
 
 package edu.nps.moves.mmowgliMobile;
 
+import com.vaadin.addon.touchkit.ui.NavigationManager;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
+import edu.nps.moves.mmowgli.db.Game;
+import edu.nps.moves.mmowgli.hibernate.HSess;
 import edu.nps.moves.mmowgli.utility.MiscellaneousMmowgliTimer.MSysOut;
-import edu.nps.moves.mmowgliMobile.ui.MmowgliMobileMainView;
-
-//One or more of the following interferes with the session interceptor design of the hibernate container
-//@Push(value=PushMode.AUTOMATIC,transport=Transport.LONG_POLLING)
-//@PreserveOnRefresh
+import edu.nps.moves.mmowgliMobile.ui.GameDataCategoriesView2;
+import edu.nps.moves.mmowgliMobile.ui.NotAllowedPopover;
 
 @SuppressWarnings("serial")
 @Theme("mmowglimobile")
-//@Theme("touchkit")
 @Title("mmowgli")
 @Widgetset("edu.nps.moves.mmowgliMobile.gwt.MmowgliMobileWidgetSet")
 
 public class MmowgliMobileUI extends UI
 {
- // private SigninPopover signinPopover = new SigninPopover();
- 
-  public MmowgliMobileUI()
-  {
-  }
-
   @Override
   protected void init(VaadinRequest request)
   {
-    MSysOut.println("MM mmowgli mobile UI init");
-    setContent(new MmowgliMobileMainView());
-    setImmediate(true);
- /* this adds signin:    
-    if(signinPopover != null) {
-      addWindow(signinPopover);
-      signinPopover.center();
-    } */
-  }
-
+    MSysOut.println("MM mmowgli mobile UI init v2");
+    setContent(new NavigationManager(new GameDataCategoriesView2()));
+    
+    HSess.init();
+    Game g = Game.get(HSess.get());
+    HSess.close();
+    
+    if(g.getCurrentMove().getCurrentMovePhase().isLoginAllowGuests())
+      ;
+    else {
+      NotAllowedPopover pop = new NotAllowedPopover();
+      addWindow(pop);
+      pop.center();
+    }
+  } 
 }
