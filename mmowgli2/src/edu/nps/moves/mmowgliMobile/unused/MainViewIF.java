c93b6c3d5f9573fc1288a59e04f827338fefa5ee
jmbailey <jmbailey@nps.edu>
2014-10-31 23:09:15 +0000

    not used

---
+++ mmowgli2/src/edu/nps/moves/mmowgliMobile/unused/MainViewIF.java
@@ -0,0 +1,9 @@
+package edu.nps.moves.mmowgliMobile.unused;
+
+import edu.nps.moves.mmowgliMobile.data.ListEntry;
+
+public interface MainViewIF
+{
+  public void setListItem(ListEntry message, ListView messageHierarchyView);
+  public void updateNewListItems();
+}
