fbd974230909c2da874ac58fea2906e06130c6b5
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-11-03 21:40:26 +0000

    Add new GNU license

--- mmowgli2/src/edu/nps/moves/mmowgliMobile/ui/ActionPlanRenderer2.java
+++ mmowgli2/src/edu/nps/moves/mmowgliMobile/ui/ActionPlanRenderer2.java
@@ -1,12 +1,34 @@
+/*
+  Copyright (C) 2010-2014 Modeling Virtual Environments and Simulation
+  (MOVES) Institute at the Naval Postgraduate School (NPS)
+  http://www.MovesInstitute.org and http://www.nps.edu
+ 
+  This file is part of Mmowgli.
+  
+  Mmowgli is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  Mmowgli is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Mmowgli in the form of a file named COPYING.  If not,
+  see <http://www.gnu.org/licenses/>
+*/
+
 package edu.nps.moves.mmowgliMobile.ui;
 
 import com.vaadin.addon.touchkit.ui.NavigationManager;
 import com.vaadin.addon.touchkit.ui.NavigationView;
-import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 
+import edu.nps.moves.mmowgli.components.Hr;
 import edu.nps.moves.mmowgli.db.ActionPlan;
 import edu.nps.moves.mmowgli.db.User;
 import edu.nps.moves.mmowgli.hibernate.HSess;
@@ -68,7 +90,7 @@ public class ActionPlanRenderer2 extends EntryRenderer2
   }
   private Component makeHr()
   {
-    Label lab = new Label("<hr/>", ContentMode.HTML);
+    Label lab = new Hr();
     lab.addStyleName("m-actionplan-hr");
     return lab;
   }
--- mmowgli2/src/edu/nps/moves/mmowgliMobile/ui/CardRenderer2.java
+++ mmowgli2/src/edu/nps/moves/mmowgliMobile/ui/CardRenderer2.java
@@ -1,3 +1,25 @@
+/*
+  Copyright (C) 2010-2014 Modeling Virtual Environments and Simulation
+  (MOVES) Institute at the Naval Postgraduate School (NPS)
+  http://www.MovesInstitute.org and http://www.nps.edu
+ 
+  This file is part of Mmowgli.
+  
+  Mmowgli is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  Mmowgli is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Mmowgli in the form of a file named COPYING.  If not,
+  see <http://www.gnu.org/licenses/>
+*/
+
 package edu.nps.moves.mmowgliMobile.ui;
 
 import java.text.SimpleDateFormat;
@@ -93,14 +115,14 @@ public class CardRenderer2 extends EntryRenderer2 //implements ClickListener
 
     cardLay.addComponent(horl);
     
-//    lbl = new Label("<hr/>", ContentMode.HTML);   
+//    lbl = new Hr();   
 //    layout.addComponent(lbl);
     
     lbl = new Label("Child Cards");
     layout.addComponent(lbl);
     lbl.addStyleName("m-text-center");
     
-//    lbl = new Label("<hr/>", ContentMode.HTML);
+//    lbl = new Hr();
 //    layout.addComponent(lbl);
 
     horl = new HorizontalLayout();
