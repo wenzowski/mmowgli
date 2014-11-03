502fb72f59321e2fc2a1b6bd03e6673d4d87b7d1
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-11-03 21:42:56 +0000

    cleanup

--- mmowgli2/src/edu/nps/moves/mmowgliMobile/unused/MainViewIF.java
+++
@@ -1,9 +0,0 @@
-package edu.nps.moves.mmowgliMobile.unused;
-
-import edu.nps.moves.mmowgliMobile.data.ListEntry;
-
-public interface MainViewIF
-{
-  public void setListItem(ListEntry message, ListView messageHierarchyView);
-  public void updateNewListItems();
-}
