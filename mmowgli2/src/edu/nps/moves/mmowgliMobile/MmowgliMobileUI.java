8c7ad5b8abd970c5f664a9abd78681ca511621d2
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-11-03 21:41:03 +0000

    Add new GNU license

--- mmowgli2/src/edu/nps/moves/mmowgliMobile/MmowgliMobileUI.java
+++ mmowgli2/src/edu/nps/moves/mmowgliMobile/MmowgliMobileUI.java
@@ -1,21 +1,23 @@
 /*
-  Copyright (C) 2014 Modeling Virtual Environments and Simulation (MOVES)
-  Institute at the Naval Postgraduate School (NPS)
-  (http://www.MovesInstitute.org and http://www.nps.edu)
+  Copyright (C) 2010-2014 Modeling Virtual Environments and Simulation
+  (MOVES) Institute at the Naval Postgraduate School (NPS)
+  http://www.MovesInstitute.org and http://www.nps.edu
  
-  This program is free software: you can redistribute it and/or modify
+  This file is part of Mmowgli.
+  
+  Mmowgli is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   any later version.
 
-  This program is distributed in the hope that it will be useful,
+  Mmowgli is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program, in the form of files named COPYING and
-  COPYING.LESSER.  If not, see <http://www.gnu.org/licenses/>
+  along with Mmowgli in the form of a file named COPYING.  If not,
+  see <http://www.gnu.org/licenses/>
 */
 
 package edu.nps.moves.mmowgliMobile;
