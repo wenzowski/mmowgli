cee93fed85dde6434548b1efee1736ebef9764ff
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-11-03 21:41:40 +0000

    Add new GNU license

--- mmowgli2/src/edu/nps/moves/security/UrlCheckingScript.sh
+++ mmowgli2/src/edu/nps/moves/security/UrlCheckingScript.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 #
+#  Copyright (C) 2010-2014 Modeling Virtual Environments and Simulation
+#  (MOVES) Institute at the Naval Postgraduate School (NPS)
+#  http://www.MovesInstitute.org and http://www.nps.edu
+ 
+#  This file is part of Mmowgli.
+  
+#  Mmowgli is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  any later version.
+
+#  Mmowgli is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with Mmowgli in the form of a file named COPYING.  If not,
+#  see <http://www.gnu.org/licenses/>
+
 # Peforms at least a perfunctory check on the kosherness
 # of URLs posted to the game. Uses the Spamhaus XBL list,
 # a list of hosts that are known to be hijacked. See
