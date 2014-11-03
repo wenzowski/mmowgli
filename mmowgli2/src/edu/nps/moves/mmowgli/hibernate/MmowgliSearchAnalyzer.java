5718a331fd9a7f5b58ba949dabc3ecfc4a1fa18a
jmbailey <jmbailey@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2014-11-03 21:33:28 +0000

    Add new GNU license

--- mmowgli2/src/edu/nps/moves/mmowgli/hibernate/MmowgliSearchAnalyzer.java
+++ mmowgli2/src/edu/nps/moves/mmowgli/hibernate/MmowgliSearchAnalyzer.java
@@ -1,36 +1,25 @@
 /*
-* Copyright (c) 1995-2010 held by the author(s).  All rights reserved.
-*  
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-*  
-*  * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*  * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer
-*       in the documentation and/or other materials provided with the
-*       distribution.
-*  * Neither the names of the Naval Postgraduate School (NPS)
-*       Modeling Virtual Environments and Simulation (MOVES) Institute
-*       (http://www.nps.edu and http://www.MovesInstitute.org)
-*       nor the names of its contributors may be used to endorse or
-*       promote products derived from this software without specific
-*       prior written permission.
-*  
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
+  Copyright (C) 2010-2014 Modeling Virtual Environments and Simulation
+  (MOVES) Institute at the Naval Postgraduate School (NPS)
+  http://www.MovesInstitute.org and http://www.nps.edu
+ 
+  This file is part of Mmowgli.
+  
+  Mmowgli is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  Mmowgli is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Mmowgli in the form of a file named COPYING.  If not,
+  see <http://www.gnu.org/licenses/>
 */
+
 package edu.nps.moves.mmowgli.hibernate;
 
 import java.io.Reader;
