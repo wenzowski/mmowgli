b076e846809abf338c26d5a8eba73fec610d66de
mcgredo <mcgredo@nps.edu>
2015-06-05 18:59:38 +0000

    Parameterized passwords

--- hostConfiguration/trunk/mmowgliVagrant/manifests/modules/mmowgli_mysql/manifests/init.pp
+++ hostConfiguration/trunk/mmowgliVagrant/manifests/modules/mmowgli_mysql/manifests/init.pp
@@ -6,7 +6,7 @@ include mysql::client
 # Set up users and databases. 
 mysql::db {"mmowgli":
   user => "mmowgli",
-  password => "gwtservelet",
+  password => "$mmowgli_database_application_password",
   host => "localhost",
   grant => "ALL",
   sql => "/InstallFiles/mmowgli_bootstrap.sql",
@@ -17,7 +17,7 @@ mysql::db {"mmowgli":
 # values in the /etc/my.cnf file, or the "show variables" settings.
 
 class {"mysql::server":
-  root_password => "mmowgli",
+  root_password => "$mmowgli_database_root_password",
   remove_default_accounts => true,
   override_options => {'mysqld' => {'max_connections' => '1024', 'default-storage-engine' => 'innodb', 'query_cache_size' => '20M', 'key_buffer_size' => '10M' } },
 }
